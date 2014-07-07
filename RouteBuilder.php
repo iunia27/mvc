5b533d11726a38b260e1468149edd9e89d86f097
Catalisan Alexandru <alexandru.catalisan@softvision.ro>
2014-07-07 16:43:57 +0300

    commit new nasmes

---
+++ RouteBuilder.php
@@ -0,0 +1,47 @@
+<?php
+	include_once 'Route.php';
+	
+	class RouterBuilder{
+		
+		private static $default_controller = 'Home';
+		private static $default_action = 'Index';
+		private static $default_params;
+		
+		public function __construct(){}
+		
+		public function createRoute($url){
+			if (isset($url)){
+				$parsed_url = parse_url($url);
+				$route_values = explode('/', $parsed_url['path']);
+				if (isset($route_values) && is_array($route_values)){
+					$route_count = count($route_values);
+					$controller = $route_values[1];
+					$action = $this->getAction();
+					$params = $this->getParameters();
+					return new Route($controller, $action, $params);
+				}
+			}
+			return false;
+		}
+		
+		private function getAction($route_count, $route_values){
+			$action;
+			if ($route_count > 2){
+				$action = $route_values[2];
+			} else {
+				$action = $this->$default_action;
+			}
+			return $action;
+		}
+		
+		private function getParamaters($route_count, $route_values){
+			$params = new array();
+			if ($route_count > 3){
+				for($i = 3; $i < $route_count = count($route_values); $i++){
+					array_push($params, $route_values[$i]);
+				}
+			}
+			return $params;
+		}
+	}
+?>
