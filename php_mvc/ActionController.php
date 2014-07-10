1241339b2dbc199a93150afb2d79d5c07892845f
iunia <iunia.bujita@softvision.ro>
2014-07-10 17:06:21 +0300

    action controller modified

--- php_mvc/ActionController.php
+++ php_mvc/ActionController.php
@@ -1,27 +1,13 @@
 <?php
 
-class ActionController{
-    protected $_model;
-    protected $controller;
-    protected $action;
-    protected $view;
-    
-    function __construct($model,$controller,$action){
-        $this->_model = $model;
-        $this->action = $action;
-        $this->controller = $controller;
+abstract class ActionController {
+
+    function __construct() {
         
-        $this->$model = new $model;
-        $this->view = new View($controller,$action);
     }
-    
-    function set($name,$value){
-        $this->view->set($name,$value);
-    }
-    
-    function __destruct() {
-        $this->view->render();
-    }
+
+    abstract function index();
+
 }
 
 ?>
