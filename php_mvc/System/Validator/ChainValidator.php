6b2e5cda72c95f635da0b1df541e8cd79020f1bd
Catalisan Alexandru <alexandru.catalisan@softvision.ro>
2014-07-24 17:30:10 +0300

    chain validator class

--- php_mvc/System/Validator/ChainValidator.php
+++ php_mvc/System/Validator/ChainValidator.php
@@ -8,22 +8,64 @@
 		/*
 		* Attaches a new validator at the end of the queue.
 		*/
-		public function addValidator(IValidator $validator, $breakChainOnFailure = false){
-		
+		public function addValidator(IValidator $validator, $breakChainOnFailure = false, $priority = 0){
+			$key = get_class($validator);
+			if (!array_key_exists($key, $this->validators)) {
+				if ($priority){
+					$this->preAppendValidator($validator, $breakChainOnFailure);
+				}else{
+					$element = array('validator' => $validator, 'breakChain' => $breakChainOnFailure);
+					$this->validators = array_push_assoc($this->validators, $key, $element);
+				}
+			}
 		}
 
 		/*
 		* Attaches a new validator at the begining of the queue.
 		*/
 		public function preAppendValidator(IValidator $validator, $breakChainOnFailure = false){
-		
+			$key = get_class($validator);
+			if (!array_key_exists($key, $this->validators)) {
+				$element = array($key => array('validator' => $validator, 'breakChain' => $breakChainOnFailure));
+				$this->validators = $element + $this->validators;
+			}
+		}
+
+		/*
+		* instantiates validators based on their names.
+		* param $validators represents an array containing validators name.
+		*/
+		public function loadValidators($validators){
+			if (is_array($validators)){
+				foreach($validators as $value){
+					$this->loadValidator($value);	
+				}
+			}elseif (is_string($validators)){
+				$this->loadValidator($validators);
+			}
+		}
+
+		/*
+		* instantiates a validator based on its name.
+		* param $validator represent string containig validator name.
+		*/
+		public function loadValidator($validator){
+			if(is_string($validator)){
+				$validatorInstance = new $validator();
+				$this->addValidator($validatorInstance, false, 0);
+			}else{
+				die("The required validator doesn't exists");
+			}
 		}
 		
 		/*
 		* Detach a validator based on validator name which represents the associative array key.
 		*/
-		public function dettachValidator($validatorKeyName){
-		
+		public function dettachValidator($validator){
+			$key = get_class($validator);
+			if (array_key_exists($key, $this->validators)){
+				unset($this->validators[$validatorKeyName]);
+			}
 		}
 		
 		/*
@@ -43,7 +85,7 @@
 		/*
 		* Returns true if and only if $value passes all validations in the chain
 		*/
-		public function isValid($value, $context){
+		public function isValid($value){
 			
 		}
 		
