b2ac02f36d5501f78578e17ad8e43eb230c37979
Catalisan Alexandru <alexandru.catalisan@softvision.ro>
2014-07-24 12:02:01 +0300

    chain validator

---
+++ php_mvc/System/Validator/ChainValidator.php
@@ -0,0 +1,56 @@
+<?php
+	
+	class ChainValidator{
+		
+		private $messages = array();
+		private $validators = array();
+		
+		/*
+		* Attaches a new validator.
+		*/
+		public function attachValidator(IValidator $validator, $breakChainOnFailure = false){
+		
+		}
+		
+		/*
+		* Detach a validator based on validator name which represents the associative array key.
+		*/
+		public function dettachValidator($validatorKeyName){
+		
+		}
+		
+		/*
+		* Returns all validators attached since then.
+		*/
+		public function getValidators(){
+			return $this->validators;
+		}
+		
+		/*
+		* Returns all failure messages.
+		*/
+		public function getMessages(){
+			return $this->messages;
+		}
+		
+		/*
+		* Returns true if and only if $value passes all validations in the chain
+		*/
+		public function isValid($value, $context){
+			
+		}
+		
+		/*
+		* Return the count of attached validators
+		*/
+		public function countValidators(){
+			return count($this->validators);
+		}
+		
+		private function array_push_assoc($array, $key, $value){
+			$array[$key] = $value;
+			return $array;
+		}
+	}
+	
+?>
